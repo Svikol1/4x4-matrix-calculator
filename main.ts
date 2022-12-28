9b66a77372c635dcde21f78246a485b0fe572f6e
Svikol1 <99035768+Svikol1@users.noreply.github.com>
2022-12-28 23:02:43 +0100

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,73 @@
+function scanKeys () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        buildString("0")
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        calculate()
+    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        calculate()
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        calculate()
+    } else {
+        basic.clearScreen()
+    }
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        buildString("7")
+    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        buildString("8")
+    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        buildString("9")
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        calculate()
+    } else {
+        basic.clearScreen()
+    }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        buildString("4")
+    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        buildString("5")
+    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        buildString("6")
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        calculate()
+    } else {
+        basic.clearScreen()
+    }
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        buildString("1")
+    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        buildString("2")
+    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        buildString("3")
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        calculate()
+    } else {
+        basic.clearScreen()
+    }
+}
+function buildString (key: string) {
+    basic.showString(key)
+    numberString = "" + numberString + key
+}
+function clear () {
+    numberString = ""
+}
+function evaluateNumber () {
+    if (a == "") {
+        a = numberString
+        numberString = ""
+    } else if (b == "") {
+        b = numberString
+        numberString = ""
+    }
+}
 /**
  * Connect a 4x4 matrix keypad like this:
  * 
@@ -37,107 +107,39 @@
  * 
  * # = =
  */
-function scanKeys () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        buildString(".")
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        buildString("0")
-    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        evaluateNumber()
-        basic.showString("=")
-        basic.pause(50)
-        calculate()
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        evaluateNumber()
-        operator = "+"
-        basic.showString("+")
-    } else {
-        basic.clearScreen()
-    }
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        buildString("7")
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        buildString("8")
-    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        buildString("9")
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        evaluateNumber()
-        operator = "-"
-        basic.showString("-")
-    } else {
-        basic.clearScreen()
-    }
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        buildString("4")
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        buildString("5")
-    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        buildString("6")
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        evaluateNumber()
-        operator = "/"
-        basic.showString("/")
-    } else {
-        basic.clearScreen()
-    }
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        buildString("1")
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        buildString("2")
-    } else if (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        buildString("3")
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        evaluateNumber()
-        operator = "x"
-        basic.showString("x")
-    } else {
-        basic.clearScreen()
-    }
-}
-function buildString (key: string) {
-    basic.showString(key)
-    numberString = "" + numberString + key
-}
-function clear () {
-    numberString = ""
-    a = ""
-    b = ""
-    operator = ""
-}
-function evaluateNumber () {
-    if (a == "") {
-        a = numberString
-        numberString = ""
-    } else if (b == "") {
-        b = numberString
-        numberString = ""
-    }
-}
 function calculate () {
-    if (a == "" || b == "") {
-        basic.showString("e")
-    } else if (operator == "+") {
-        basic.showNumber(parseFloat(a) + parseFloat(b))
-    } else if (operator == "-") {
-        basic.showNumber(parseFloat(a) - parseFloat(b))
-    } else if (operator == "x") {
-        basic.showNumber(parseFloat(a) * parseFloat(b))
-    } else if (operator == "/") {
-        basic.showNumber(parseFloat(a) / parseFloat(b))
+    if (parseFloat(numberString) % 5 == 1) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # # # .
+            # . # . #
+            `)
+    } else if (parseFloat(numberString) % 5 == 2 || parseFloat(numberString) % 5 == 3) {
+        basic.showLeds(`
+            . # # . .
+            # . . # #
+            . # # . .
+            # . . # .
+            . # # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # . # . #
+            # . # . #
+            # # # # #
+            `)
     }
+    basic.pause(1000)
+    basic.showNumber(parseFloat(numberString) % 5)
 }
 let b = ""
 let a = ""
 let numberString = ""
-let operator = ""
+basic.showString("Fregio di Natale")
 clear()
 basic.forever(function () {
     while (true) {
@@ -147,9 +149,7 @@ basic.forever(function () {
             clear()
         }
         if (input.buttonIsPressed(Button.B)) {
-            basic.showString(a)
-            basic.showString(operator)
-            basic.showString(b)
+            basic.showString(numberString)
         }
     }
 })
